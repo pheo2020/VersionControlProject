9c74d396d619317400015d6a795b5fc835957754
pheo2020 <b9046248@newcastle.ac.uk>
2021-02-08 00:25:01 +0000

    Added hello world file with print statement

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!!!");
+        sayHello("Jordan");
+    }
+
+
+
+    public static void sayHello(String name){
+
+                System.out.println("Hello"+ name);
+
+    }
+
+}
